f5612762fffc37fe8bee4331bf5139de8c8351ef
roflerbtw <2fz52xzwcf@privaterelay.appleid.com>
2026-02-11 00:10:31 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const mainText = document.getElementById("mainText");
+const asideText = document.getElementById("asideText");
+
+// mainText.textContent = ;
+// asideText.textContent = "А тут типа фотки можно будет сделать?";
